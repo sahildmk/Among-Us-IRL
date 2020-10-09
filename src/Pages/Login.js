480d7da98a2e09a46e6f456c141aad4cd07cb5ce
Sahil Deshmukh <sahildmk@gmail.com>
2020-10-09 20:04:58 +1100

    Start

---
+++ src/Pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './L'
+
+function LoginPage() {
+    return (
+        <div>
+            <form>
+                <label name="userName">Username: </label>
+                <input type="text"></input>
+            </form>
+        </div>
+    );
+}
+
+export default LoginPage;
