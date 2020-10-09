039ba19cfd48b93a07f8fbb503e79f7f8bc1f43e
Sahil Deshmukh <sahildmk@gmail.com>
2020-10-10 02:04:40 +1100

    login form

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import './L'
+import './LoginStyle.css';
 
 function LoginPage() {
+    const handleLogin = event => {
+        event.preventDefault();
+        const loginForm = document.forms.login;
+        const uName = loginForm.username.value;
+        const pWord = loginForm.password.value;
+    };
+
     return (
-        <div>
-            <form>
-                <label name="userName">Username: </label>
-                <input type="text"></input>
+        <div id="page">
+            <h1>Welcome!</h1>
+            <form name="login" onSubmit={handleLogin}>
+                <div id="loginForm">
+                    <div className="uName">
+                        <label name="username">Username: </label>
+                        <input name="username" type="text"></input> <br/>
+                    </div>
+                    <div className="pWord">
+                        <label name="password">Password: </label>
+                        <input name="password" type="password"></input> <br/>
+                    </div>
+                    <button id="loginBtn" type="submit">Login</button>
+                </div>
             </form>
         </div>
     );
 }
+
+
 
 export default LoginPage;
